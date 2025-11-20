216b5e112e6312b0af6ba4b3b703976793b53010
Harish <hragavendra25@gmail.com>
2025-11-20 10:05:15 +0530

    Repo Created for Tables

---
+++ src/main/java/com/example/Smart_Eye_Care_be/Repository/DoctorRepo.java
@@ -0,0 +1,10 @@
+package com.example.Smart_Eye_Care_be.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Smart_Eye_Care_be.Models.DoctorModel;
+
+@Repository
+public interface DoctorRepo extends JpaRepository<DoctorModel,Long> {
+}
