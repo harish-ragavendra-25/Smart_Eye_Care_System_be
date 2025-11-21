5a8d6b79b4b5fd3f8d7813144e1444560010e747
Harish <hragavendra25@gmail.com>
2025-11-21 10:49:26 +0530

    created ReportImage Entity

---
+++ src/main/java/com/example/Smart_Eye_Care_be/Models/ReportImage.java
@@ -0,0 +1,31 @@
+package com.example.Smart_Eye_Care_be.Models;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "report_images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imgUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report report;
+}
